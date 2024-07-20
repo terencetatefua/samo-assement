72cca234dd3be7295269a73d2648c982a6eaf094
terencetatefua <terencetatefua@gmail.com>
2024-07-19 19:01:04 -0500

    updated module

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -128,12 +128,6 @@ resource "aws_lb_listener" "main" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "ecs_service" {
-  target_group_arn = aws_lb_target_group.main.arn
-  target_id        = var.ecs_service_ip  # Ensure this is set to the correct IP
-  port             = 80
-}
-
 resource "aws_security_group" "lb" {
   vpc_id = var.vpc_id
 
